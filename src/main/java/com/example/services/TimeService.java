3abd130d906e99ffdfb4a47441d8db073a5967a9
hightitude <arjunmuk@arjuns-mbp.home>
2014-08-11 23:01:09 -0400

    Added a time zone service

--- src/main/java/com/example/services/TimeService.java
+++ src/main/java/com/example/services/TimeService.java
@@ -1,19 +1,31 @@
 package com.example.services;
 
+import java.util.TimeZone;
+
 import com.example.models.Time;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/time")
 @Produces(MediaType.APPLICATION_JSON)
-public class TimeService {
+public class TimeService 
+{
 
     @GET
-    public Time get() {
+    public Time get() 
+    {
         return new Time();
+    }
+    
+    @GET
+    @Path("/{timezone}")
+    public Time get(@PathParam("timezone") String timezone) 
+    {
+        return new Time(TimeZone.getTimeZone(timezone.toUpperCase()));
     }
 
 }
