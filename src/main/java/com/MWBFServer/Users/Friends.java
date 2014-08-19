c1e33cba5804f627b36dfc347cbd25de706e06a6
hightitude <arjunmuk@arjuns-mbp.home>
2014-08-19 01:41:38 -0400

    Added Friends

---
+++ src/main/java/com/MWBFServer/Users/Friends.java
@@ -0,0 +1,74 @@
+package com.MWBFServer.Users;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table (name="USER_FRIENDS")
+public class Friends 
+{
+	private long id;
+	private User user;
+	private User friend;
+	
+	protected Friends(){}
+	
+	/**
+	 * 
+	 * @param _user
+	 * @param _friend
+	 */
+	public Friends(User _user, User _friend)
+	{
+		user = _user;
+		friend = _friend;
+	}
+	
+	@Id @GeneratedValue
+	@Column (name="ID")
+	public long getId() 
+	{
+		return id;
+	}
+	public void setId(long _id) 
+	{
+		this.id = _id;
+	}
+	
+	
+	@ManyToOne
+    @JoinColumn(name = "user_id")
+	public User getUser() 
+	{
+		return user;
+	}
+	public void setUser(User _user) 
+	{
+		this.user = _user;
+	}
+	
+	
+	@ManyToOne
+    @JoinColumn(name = "friend_user_id")
+	public User getFriend() 
+	{
+		return friend;
+	}
+	public void setFriend(User _friend) 
+	{
+		this.friend = _friend;
+	}
+	
+
+	@Override
+	public String toString()
+	{
+		return "User : [" + user.getId() + "], FriendsList [" + friend.getId() + "]";
+	}
+}
