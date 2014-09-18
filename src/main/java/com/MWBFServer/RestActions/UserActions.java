711ff484b6cd4d5293d2dcf2bba52da2e04cad27
Vivek Pasari <vpasari@gmail.com>
2014-09-17 21:28:49 -0400

    Added a log for testing

--- src/main/java/com/MWBFServer/RestActions/UserActions.java
+++ src/main/java/com/MWBFServer/RestActions/UserActions.java
@@ -232,6 +232,9 @@ public class UserActions
 		String returnStr = null;
 		Gson gson = new Gson();
 		
+		//TODO: remove this
+		log.warn("Vivek's test warning");
+		
 		Type collectionType = new TypeToken<List<UserActivity>>(){}.getType();
 		List<UserActivity> newActivityList = gson.fromJson(_incomingData, collectionType);
 		
