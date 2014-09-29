3728c124da15972da80e3897d2bbc6d989d0aa7b
hightitude <arjunmuk@arjuns-mbp.home>
2014-09-29 13:37:09 -0400

    Increased feed to 100

--- src/main/java/com/MWBFServer/Utils/Constants.java
+++ src/main/java/com/MWBFServer/Utils/Constants.java
@@ -3,5 +3,5 @@ package com.MWBFServer.Utils;
 public final class Constants 
 {
 	// The maximum number of messages to display in a users feed
-	static int MAX_NUMBER_OF_MESSAGE_FEEDS = 50;
+	static int MAX_NUMBER_OF_MESSAGE_FEEDS = 100;
 }
