0da84d27f5b3d3ca84cf7fed5f2f83cff2253ea0
hightitude <arjunmuk@arjuns-mbp.home>
2015-07-01 22:41:36 -0400

    Refactored code from Utils to respective classes

--- src/main/java/com/MWBFServer/RestActions/UserActions.java
+++ src/main/java/com/MWBFServer/RestActions/UserActions.java
@@ -77,7 +77,7 @@ public class UserActions
 			{
 				log.info("First time FaceBook User Registering [" + email + "]");
 				User newUser = new User(email,firstName,lastName,profileId);
-				returnStr = Utils.addUser(newUser);
+				returnStr = User.addUser(newUser);
 			}
 		}
 		
@@ -116,7 +116,7 @@ public class UserActions
 		}
 		else
 			// If successful, add to the local cache and DB
-			returnStr = Utils.addUser(newUser);
+			returnStr = User.addUser(newUser);
 		
 		return BasicUtils.buildResponse(returnStr);
 	}
@@ -222,7 +222,7 @@ public class UserActions
             newActivityList = gson.fromJson(_incomingData, collectionType);
             
             // If successful, add to the local cache
-    		if ( Utils.logActivity(newActivityList) )
+    		if ( UserActivity.logActivity(newActivityList) )
     			returnStr = BasicUtils.constructReturnString(JsonConstants.SUCCESS_YES, "Activity logged.");
     		else
     		{
@@ -389,7 +389,7 @@ public class UserActions
 		else
 		{
 			// Delete all of the users activities
-			if ( !Utils.deleteActivity(activityId) )
+			if ( !UserActivity.deleteActivity(activityId) )
 				returnStr = BasicUtils.constructReturnString(JsonConstants.SUCCESS_NO, "Unable to delete user activity.");
 			else
 				returnStr = BasicUtils.constructReturnString(JsonConstants.SUCCESS_YES, "Deleted the users activity.");
@@ -723,7 +723,7 @@ public class UserActions
 			log.info("Adding new challenge [" + newChallenge.toString() + "]");
 			
 			// If successful, add to the local cache
-			if ( Utils.addChallenge(newChallenge) )
+			if ( Challenge.addChallenge(newChallenge) )
 				returnStr = BasicUtils.constructReturnString(JsonConstants.SUCCESS_YES, "New challenge added.");
 			else
 			{
@@ -757,7 +757,7 @@ public class UserActions
 			log.info("Deleting  challenge [" + challengeId + "]");
 			
 			// If successful, add to the local cache
-			if ( Utils.deleteChallenge(challengeId) )
+			if ( Challenge.deleteChallenge(challengeId) )
 				returnStr = BasicUtils.constructReturnString(JsonConstants.SUCCESS_YES, "Challenge deleted.");
 			else
 			{
